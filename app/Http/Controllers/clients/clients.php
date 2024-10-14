d519e2e1f781277ad423f453ab8c0f7bfffa05d9
deiker619 <deiker1842@gmail.com>
2024-10-14 19:38:11 -0400

    client store, destroy

--- app/Http/Controllers/clients/clients.php
+++ app/Http/Controllers/clients/clients.php
@@ -12,13 +12,36 @@ class clients extends Controller
 
     public function store(Request $request)
     {
-        $client = Client::create($request->all());
-        return response()->json($client);
+
+
+        if (Client::where('cedula', $request->cedula)->exists()) { //El Cliente ya esta registrado
+            $client = Client::where('cedula', $request->cedula)->get();
+            return response()->json([
+                'message' => 'Bienvenido de nuevo',
+                'client' => $client
+            ]);
+        } else {
+            $client = Client::create($request->all());
+            return response()->json([
+                'message' => 'Registro exitoso',
+                'client' => $client
+            ]);
+        } 
     }
 
-    public function destroy() {}
+    public function destroy(Request $request)
+    {
+        $client = Client::findOrFail($request->id);
+        $client->delete();
+    }
 
-    public function update() {}
+    public function update(Request $request)
+    {
+        $client = Client::findOrFail($request->id);
+        $client->fill($request->all());
+        $client->save();
+        return response()->json($client, 200);
+    }
 
     public function edit() {}
 }
